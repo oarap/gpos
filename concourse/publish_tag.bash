0d0db7982f9cac6a9839cc02870d19bd41ff8200
Jesse Zhang and Omer Arap <oarap@pivotal.io>
2016-06-21 17:10:40 -0700

    Compile 32 bit release type [#121003303]

--- concourse/publish_tag.bash
+++ concourse/publish_tag.bash
@@ -7,7 +7,7 @@ set -u -e -x
 main() {
   local INPUT_RLS_DIR=bin_gpos_release
   local INPUT_DBG_DIR=bin_gpos_debug
-  local INPUT_OSX_DIR=bin_gpos_osx_debug
+  local INPUT_OSX_DIR=bin_gpos_osx_release
   local OUTPUT_DIR=gpos_github_release_stage
   env LC_ALL=C tar tf $INPUT_RLS_DIR/*.tar.gz | grep "libgpos.so." | sort -n | head -n 1 | sed 's/\.\/lib\/libgpos\.so\./v/' > $OUTPUT_DIR/tag.txt
   cp -v $INPUT_RLS_DIR/*.tar.gz $OUTPUT_DIR/
